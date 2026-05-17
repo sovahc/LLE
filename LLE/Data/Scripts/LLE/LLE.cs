a1af09c103a209050a17be8a36afbaa4f983a905
cat <cat@meow>
2026-05-17 18:08:40 +0200

    merged

--- LLE/Data/Scripts/LLE/LLE.cs
+++ LLE/Data/Scripts/LLE/LLE.cs
@@ -253,14 +253,6 @@ namespace LLE
 			var pm = player.Character.GetHeadMatrix(false);
 			//Vision.HighlightVisible(pm.Translation, pm.Forward);
 
-			if (grid_A != null)
-			{
-				foreach(var v in Map.debug)
-				{	Vector3D world = grid_A.GridIntegerToWorld(v + grid_A.Min - 1);
-					Drawing.RoundMarker(world, Color.Gray);
-				}
-			}
-
 			MyConsole.Render(font);
 
 			Common.Call_Add_Billboards(); // just for sure
