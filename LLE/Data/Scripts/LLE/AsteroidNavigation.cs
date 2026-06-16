ff740e36c99e8dfa34feaffa11667ee277d23fe3
cat <cat@meow>
2026-06-16 13:13:40 +0200

    ref

--- LLE/Data/Scripts/LLE/AsteroidNavigation.cs
+++ LLE/Data/Scripts/LLE/AsteroidNavigation.cs
@@ -39,6 +39,8 @@ namespace LLE
 		private readonly int _coarseLod;
 		private readonly OctreeNode _root;
 
+		private static readonly MyStorageData storage = new MyStorageData();
+
 		public struct Statistic_
 		{	public int Unknown, Top, Free, Mixed, Blocked;
 
@@ -68,27 +70,120 @@ namespace LLE
 
 		public OctreeNode GetNodeAt(Vector3I pos)
 		{
-			var nodes = GetNodesInRange(pos, pos);
-			return nodes.Count > 0 ? nodes[0] : null;
+			if (_voxel == null || _voxel.MarkedForClose) return null;
+
+			var result = new List<OctreeNode>();
+			CollectInRange(_root, pos, pos, result);
+
+			return result.Count > 0 ? result[0] : null;
 		}
 
-		/// <summary>
-		/// Finds the node containing a point. Traverses from root down.
-		/// </summary>
-		public OctreeNode FindNodeAt(Vector3I pos)
+		private void CollectInRange(OctreeNode node, Vector3I queryMin, Vector3I queryMax, List<OctreeNode> result)
 		{
-			var current = _root;
-			while (current.Children != null)
+			var nodeMax = node.Min + node.Size - 1;
+
+			if (node.Min.X > queryMax.X || nodeMax.X < queryMin.X ||
+			    node.Min.Y > queryMax.Y || nodeMax.Y < queryMin.Y ||
+			    node.Min.Z > queryMax.Z || nodeMax.Z < queryMin.Z)
+				return;
+
+			if (node.Type == NodeType.Unknown)
 			{
-				var half = current.Size / 2;
-				bool x = pos.X >= current.Min.X + half;
-				bool y = pos.Y >= current.Min.Y + half;
-				bool z = pos.Z >= current.Min.Z + half;
-				
-				int index = (x ? 1 : 0) | (y ? 2 : 0) | (z ? 4 : 0);
-				current = current.Children[index];
+				--Statistic.Unknown;
+				int lod = CalculateLodLevel(node.Size);
+
+				if (lod >= _coarseLod)
+					node.Type = NodeType.Top;
+				else
+				{
+					var voxelMin = node.Min;
+					var voxelMax = node.Min + node.Size - 1;
+					node.Type = VoxelCellType(_voxel, voxelMin, voxelMax, lod - 1);
+					Utilities.Log($"VoxelCellType {node.Min} / {voxelMin}-{voxelMax} / {lod} / {node.Type}");
+				}
+
+				switch(node.Type)
+				{
+					case NodeType.Top: ++Statistic.Top; break;
+					case NodeType.Free: ++Statistic.Free; break;
+					case NodeType.Mixed: ++Statistic.Mixed; break;
+					case NodeType.Blocked: ++Statistic.Blocked; break;
+				}
 			}
-			return current;
+
+			if (node.Type != NodeType.Mixed && node.Type != NodeType.Top)
+			{
+				result.Add(node);
+				return;
+			}
+
+			if (node.Children == null)
+				node.Children = new OctreeNode[8];
+
+			var half = node.Size / 2;
+			for (int i = 0; i < 8; i++)
+			{
+				int ix = (i & 1) != 0 ? half : 0;
+				int iy = (i & 2) != 0 ? half : 0;
+				int iz = (i & 4) != 0 ? half : 0;
+
+				var childMin = node.Min + new Vector3I(ix, iy, iz);
+				var childMax = childMin + half - 1;
+
+				// Skip allocation if child is outside query range
+				if (childMin.X > queryMax.X || childMax.X < queryMin.X ||
+				    childMin.Y > queryMax.Y || childMax.Y < queryMin.Y ||
+				    childMin.Z > queryMax.Z || childMax.Z < queryMin.Z)
+					continue;
+
+				if (node.Children[i] == null)
+				{
+					node.Children[i] = new OctreeNode { Min = childMin, Size = half, Type = NodeType.Unknown };
+					++Statistic.Unknown;
+				}
+
+				CollectInRange(node.Children[i], queryMin, queryMax, result);
+			}
+		}
+
+		public static NodeType VoxelCellType(MyVoxelBase voxel, Vector3I min, Vector3I max, int lod = 0)
+		{
+			Vector3I storageMax = voxel.Storage.Size - 1;
+			Vector3I coordMin = min >> lod;
+			Vector3I coordMax = max >> lod;
+
+			//Vector3I.Clamp(ref coordMin, ref Vector3I.Zero, ref storageMax, out coordMin);
+			//Vector3I.Clamp(ref coordMax, ref Vector3I.Zero, ref storageMax, out coordMax);
+
+			storage.Resize(coordMin, coordMax);
+			voxel.Storage.ReadRange(storage, MyStorageDataTypeFlags.Material, lod, coordMin, coordMax);
+
+			Utilities.Log(storage.ToBase64());
+		
+			Vector3I offset = Vector3I.Zero;
+
+			var index = storage.ComputeLinear(ref offset);
+			byte v0 = storage.Material(index);
+
+			Vector3I p;
+			for (p.X = coordMin.X; p.X <= coordMax.X; p.X++)
+			{
+				for (p.Y = coordMin.Y; p.Y <= coordMax.Y; p.Y++)
+				{
+					for (p.Z = coordMin.Z; p.Z <= coordMax.Z; p.Z++)
+					{
+						offset = p - coordMin;
+						index = storage.ComputeLinear(ref offset);
+
+						var v = storage.Material(index);
+						if(v != v0)
+							return NodeType.Mixed;
+					}
+				}
+			}
+
+			if (v0 == byte.MaxValue) return NodeType.Free;
+			return NodeType.Blocked;
 		}
 
 		/// <summary>
@@ -191,125 +286,6 @@ namespace LLE
 				if (!parent.TryGetValue(current, out current)) break;
 			}
 			result.Reverse();
-		}
-
-		private static readonly MyStorageData storage = new MyStorageData();
-
-		public static NodeType VoxelCellType(MyVoxelBase voxel, Vector3I min, Vector3I max, int lod = 0)
-		{
-			Vector3I storageMax = voxel.Storage.Size - 1;
-			Vector3I coordMin = min >> lod;
-			Vector3I coordMax = max >> lod;
-
-			//Vector3I.Clamp(ref coordMin, ref Vector3I.Zero, ref storageMax, out coordMin);
-			//Vector3I.Clamp(ref coordMax, ref Vector3I.Zero, ref storageMax, out coordMax);
-
-			storage.Resize(coordMin, coordMax);
-			voxel.Storage.ReadRange(storage, MyStorageDataTypeFlags.Material, lod, coordMin, coordMax);
-
-			Utilities.Log(storage.ToBase64());
-		
-			Vector3I offset = Vector3I.Zero;
-
-			var index = storage.ComputeLinear(ref offset);
-			byte v0 = storage.Material(index);
-
-			Vector3I p;
-			for (p.X = coordMin.X; p.X <= coordMax.X; p.X++)
-			{
-				for (p.Y = coordMin.Y; p.Y <= coordMax.Y; p.Y++)
-				{
-					for (p.Z = coordMin.Z; p.Z <= coordMax.Z; p.Z++)
-					{
-						offset = p - coordMin;
-						index = storage.ComputeLinear(ref offset);
-
-						var v = storage.Material(index);
-						if(v != v0)
-							return NodeType.Mixed;
-					}
-				}
-			}
-
-			if (v0 == byte.MaxValue) return NodeType.Free;
-			return NodeType.Blocked;
-		}
-
-		public List<OctreeNode> GetNodesInRange(Vector3I min, Vector3I max)
-		{
-			if (_voxel == null || _voxel.MarkedForClose) return new List<OctreeNode>();
-
-			var result = new List<OctreeNode>();
-			CollectInRange(_root, min, max, result);
-			return result;
-		}
-
-		private void CollectInRange(OctreeNode node, Vector3I queryMin, Vector3I queryMax, List<OctreeNode> result)
-		{
-			var nodeMax = node.Min + node.Size - 1;
-
-			if (node.Min.X > queryMax.X || nodeMax.X < queryMin.X ||
-			    node.Min.Y > queryMax.Y || nodeMax.Y < queryMin.Y ||
-			    node.Min.Z > queryMax.Z || nodeMax.Z < queryMin.Z)
-				return;
-
-			if (node.Type == NodeType.Unknown)
-			{
-				--Statistic.Unknown;
-				int lod = CalculateLodLevel(node.Size);
-
-				if (lod >= _coarseLod)
-					node.Type = NodeType.Top;
-				else
-				{
-					var voxelMin = node.Min;
-					var voxelMax = node.Min + node.Size - 1;
-					node.Type = VoxelCellType(_voxel, voxelMin, voxelMax, lod - 1);
-					Utilities.Log($"VoxelCellType {node.Min} / {voxelMin}-{voxelMax} / {lod} / {node.Type}");
-				}
-
-				switch(node.Type)
-				{
-					case NodeType.Top: ++Statistic.Top; break;
-					case NodeType.Free: ++Statistic.Free; break;
-					case NodeType.Mixed: ++Statistic.Mixed; break;
-					case NodeType.Blocked: ++Statistic.Blocked; break;
-				}
-			}
-
-			if (node.Type != NodeType.Mixed && node.Type != NodeType.Top)
-			{
-				result.Add(node);
-				return;
-			}
-
-			if (node.Children == null)
-				node.Children = new OctreeNode[8];
-
-			var half = node.Size / 2;
-			for (int i = 0; i < 8; i++)
-			{
-				int ix = (i & 1) != 0 ? half : 0;
-				int iy = (i & 2) != 0 ? half : 0;
-				int iz = (i & 4) != 0 ? half : 0;
-
-				var childMin = node.Min + new Vector3I(ix, iy, iz);
-				var childMax = childMin + half - 1;
-
-				// Skip allocation if child is outside query range
-				if (childMin.X > queryMax.X || childMax.X < queryMin.X ||
-				    childMin.Y > queryMax.Y || childMax.Y < queryMin.Y ||
-				    childMin.Z > queryMax.Z || childMax.Z < queryMin.Z)
-					continue;
-
-				if (node.Children[i] == null)
-				{
-					node.Children[i] = new OctreeNode { Min = childMin, Size = half, Type = NodeType.Unknown };
-					++Statistic.Unknown;
-				}
-
-				CollectInRange(node.Children[i], queryMin, queryMax, result);
-			}
 		}
 	}
 }
