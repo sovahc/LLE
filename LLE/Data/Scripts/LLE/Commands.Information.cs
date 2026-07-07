33eb1ed5910db3a5900fcdc0e0d0828ab7a77b73
cat <cat@meow>
2026-07-07 04:31:16 +0200

    more stats in Overview

--- LLE/Data/Scripts/LLE/Commands.Information.cs
+++ LLE/Data/Scripts/LLE/Commands.Information.cs
@@ -179,6 +179,17 @@ namespace LLE
 			var md = new MyMarkdown();
 			md.Append($"# {category} '{name}'");
 
+			md.Append($"Has energy: {GridHasPower(selectedGrid)}");
+			var cg = selectedGrid as MyCubeGrid;
+			if(cg.Mass > 0) md.Append($"Mass: {cg.Mass}Kg");
+			Vector3D size = cg.Max - cg.Min;
+			size *= cg.GridSize;
+			md.Append($"Linear size: {size}");
+			md.Append($"Coordinate range: Min {IJK(cg.Min)} Max {IJK(cg.Max)}");
+			md.Append($"Linear velocity: {cg.LinearVelocity.Length():F1}");
+			md.Append($"Blocks count: {cg.CubeBlocks.Count}");
+			md.Append($"Natural gravity: {cg.NaturalGravity.Length():F1}");
+			
 			var ts = MyAPIGateway.TerminalActionsHelper.GetTerminalSystemForGrid(selectedGrid);
 			terminalBlocks.Clear();
 			ts.GetBlocks(terminalBlocks);
